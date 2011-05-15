d82a24c0dc93b7baeda3feaffb033cbedcf29e07
Rhett Garber <rhettg@gmail.com>
2011-05-14 17:11:06 -0700

    some cleanup, timers

--- websocket.go
+++ websocket.go
@@ -1,18 +1,47 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
+	//"os"
+	"io/ioutil"
 	"http"
 	"websocket"
+	"time"
+	"log"
 )
 
-func ServeHello(socket *websocket.Conn) {
+func ServeWS(socket *websocket.Conn) {
 	hello := []byte("hello, world\n")
-	socket.Write(hello);
+	for {
+		//timer := time.NewTimer(1000000000);
+		
+		//fmt.Printf("Timer expired: %s\n", <- timer.C)
+		<- time.After(100000000);
+		socket.Write(hello);	
+	}
+}
+
+func ServePage(writer http.ResponseWriter, request *http.Request) {
+	//file, err := os.Open("default.do");
+	log.Println("Starting /")
+	
+	//defer file.Close();
+	contents, err := ioutil.ReadFile("websocket.html")
+	if err != nil {
+		fmt.Printf("Error opening file");
+		return
+	}
+
+	writer.Write(contents);
+
 }
 
 func main() {
-	http.Handle("/hello", websocket.Handler(ServeHello));
+	log.Println("Starting up")
+	
+	http.Handle("/", http.HandlerFunc(ServePage));
+	http.Handle("/ws", websocket.Handler(ServeWS));
+
 	err := http.ListenAndServe(":8080", nil);
 	if err != nil {
 		panic("ListenAndServe: " + err.String())
