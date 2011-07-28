1baa5e61d2431b40ec8a79b7227c00da539d31cc
Benjamin Goldenberg <benjamin@sff7u7-53.706.yelpcorp.com>
2011-07-28 14:32:21 -0700

    Add array indexing to get_deep, and test suite, that mostly works, except some comparisons are broken

--- get_deep.go
+++ get_deep.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 	"strings"
+	"strconv"
 )
 
 type JSONData interface{}
 
-func GetDeep(key string, data JSONData) (interface{}, bool) {
+func GetDeep(key string, data JSONData) (dataStep interface{}, ok bool) {
 	allKeys := strings.Split(key, ".", -1)
-	
-	dataStep := data
+	dataStep = data
 	for _, subKey := range(allKeys) {
 		// Check we have something sane we can use
 		switch dataType := dataStep.(type) {
@@ -23,13 +23,24 @@ func GetDeep(key string, data JSONData) (interface{}, bool) {
 				continue
 			
 			case []interface{}:
-				log.Fatal("don't know how to handle this one yet")
-				return nil, false
+				arrayIndex, err := strconv.Atoi(subKey)
+				if err != nil {
+					log.Println("can't interpret %v as an array index", subKey)
+					return nil, false
+				}
+				defer func() {
+					if e := recover(); e != nil {
+						log.Println("Array index out of bound")
+						dataStep = nil
+						ok = false
+					}
+				}()
+				dataStep = dataStep.([]interface{})[arrayIndex]
+				continue
 			default:
-				log.Fatal("don't konw how to handle this type: %s", dataType)
+				log.Println("don't know how to handle this type: %T", dataStep)
 				return nil, false
 		}
 	}
-	
 	return dataStep, true
 }
---
+++ get_deep_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"testing"
+	"json"
+	"io/ioutil"
+	"strings"
+	"log"
+)
+
+type getDeepTest struct {
+	key string
+	value interface{}
+	ok bool
+}
+
+var getDeepTests = []getDeepTest{
+	getDeepTest{"a", 1, true},
+	getDeepTest{"b", "foo", true},
+	getDeepTest{"not_there", nil, false},
+	getDeepTest{"c", map[string]int {"d": 2}, true},
+	getDeepTest{"c.d", 2 , true},
+	getDeepTest{"array.2", nil, false},
+	getDeepTest{"c.d.not_there", nil, false},
+	getDeepTest{"array", [2]int{2,3}, true},
+	getDeepTest{"array.1", 3, true},
+	getDeepTest{"array.foo", nil, false},
+}
+
+var jsonString = `{
+	"a": 1,
+	"b": "foo",
+	"c": {
+		"d": 2
+	},
+	"array": [2,3]
+}`
+
+func TestGetDeep(t *testing.T) {
+	var fixture JSONData
+
+	reader := strings.NewReader(jsonString)
+	jsonBytes, _ := ioutil.ReadAll(reader)	
+	err := json.Unmarshal(jsonBytes, &fixture)
+
+	if err != nil {
+		t.Errorf("Couldn't read jsonString")
+	}
+
+	log.Println("fixture is ", fixture)
+	for _, test := range getDeepTests {
+		log.Println("Evaluating ", test.key)
+		value, ok := GetDeep(test.key, fixture)
+		if ok != test.ok {
+			t.Errorf("For key '%s', expected ok = %t, but was %t", test.key, test.ok, ok)
+		}
+
+		if value != test.value {
+			t.Errorf("For key '%s', expected value = %v, but was %v", test.key, test.value, value)
+		}
+	}
+}
